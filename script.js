cf413ec852df45694464bdc2847fab976eeb3017
j2thex <jnamed@gmail.com>
2023-09-06 16:50:45 +0200

    new demo

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const wordElement = document.getElementById('animatedWord');
-    
-    wordElement.addEventListener('mouseover', function() {
-        const letters = wordElement.querySelectorAll('.letter');
-        
-        letters.forEach((letter, index) => {
-            setTimeout(() => {
-                letter.style.transform = 'scaleY(1)';
-            }, 200 * index);
+/*-----
+Spanizer
+- Wraps letters with spans, for css animations
+-----*/
+(function($) {
+    var s,
+    spanizeLetters = {
+      settings: {
+        letters: $('.js-spanize'),
+      },
+      init: function() {
+        s = this.settings;
+        this.bindEvents();
+      },
+      bindEvents: function(){
+        s.letters.html(function (i, el) {
+          //spanizeLetters.joinChars();
+          var spanizer = $.trim(el).split("");
+          return '<span>' + spanizer.join('</span><span>') + '</span>';
         });
-    });
-});
+      },
+    };
+    spanizeLetters.init();
+  })(jQuery);
