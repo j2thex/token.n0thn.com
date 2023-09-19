bbc9bb9e72eaf7596002a625831fea883e984e36
j2thex <jnamed@gmail.com>
2023-09-19 20:24:57 +0200

    stickman sticking out

--- scripts.js
+++ scripts.js
@@ -14,5 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const stickman = document.getElementById("sliding-stickman");
+    const leftArm = document.getElementById("left-arm");
+    let direction = 1;
+    let angle = 0;
 
+    function wave() {
+        angle += direction * 5;
+        if (angle >= 20 || angle <= -20) {
+            direction = -direction;
+        }
+        leftArm.setAttribute("transform", `rotate(${angle}, 50, 60)`);
+        requestAnimationFrame(wave);
+    }
+
+    wave(); // Start the waving animation
+});
 
