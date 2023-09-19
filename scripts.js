b384a3c293adaba9716aac63276cc0b4178d7a55
j2thex <jnamed@gmail.com>
2023-09-19 18:46:08 +0200

    acc fix

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const stickman = document.getElementById("stickman");
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const faqQuestions = document.querySelectorAll(".faq-question");
 
     faqQuestions.forEach(question => {
-        question.addEventListener("click", function() {
+        question.addEventListener("click", function () {
             const answer = this.nextElementSibling;
             if (answer.style.display === "block") {
                 answer.style.display = "none";
@@ -15,23 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-if (window.DeviceOrientationEvent) {
+document.getElementById('requestPermission').addEventListener('click', function () {
     if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        // This is an iOS device which requires permission
-        document.body.addEventListener('click', function() {
-            DeviceOrientationEvent.requestPermission()
-                .then(response => {
-                    if (response === 'granted') {
-                        window.addEventListener('deviceorientation', handleOrientation, true);
-                    }
-                })
-                .catch(console.error);
-        });
+        DeviceOrientationEvent.requestPermission()
+            .then(response => {
+                if (response === 'granted') {
+                    window.addEventListener('deviceorientation', handleOrientation, true);
+                }
+            })
+            .catch(console.error);
     } else {
-        // Non-iOS device
         window.addEventListener('deviceorientation', handleOrientation, true);
     }
-} else {
-    console.log("DeviceOrientation is not supported");
-}
+});
+
 
