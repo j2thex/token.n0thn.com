fc11122729ec5c3d870997b71ae97ec0b88e90eb
j2thex <jnamed@gmail.com>
2023-09-28 15:06:54 +0200

    loaded DOM for metamask switch

--- scripts/addTokenToMetamask.js
+++ scripts/addTokenToMetamask.js
@@ -1,35 +1,38 @@
-document.getElementById('addToMetaMaskBtn').addEventListener('click', async () => {
-    try {
-        // Check if MetaMask is installed
-        if (typeof window.ethereum !== 'undefined') {
-            const tokenAddress = '0x0965c81656ed324d51f466B22b56645B8FDa2C73';  // Replace with your token's address
-            const tokenSymbol = 'N0THN';  // Replace with your token's symbol
-            const tokenDecimals = 18;  // Replace with your token's decimals
-            const tokenImage = 'https://example.com/token-image.png';  // Replace with the URL to your token's image
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    document.getElementById('addToMetaMaskBtn').addEventListener('click', async () => {
+        try {
+            // Check if MetaMask is installed
+            if (typeof window.ethereum !== 'undefined') {
+                const tokenAddress = '0x0965c81656ed324d51f466B22b56645B8FDa2C73';  // Replace with your token's address
+                const tokenSymbol = 'N0THN';  // Replace with your token's symbol
+                const tokenDecimals = 18;  // Replace with your token's decimals
+                const tokenImage = 'https://example.com/token-image.png';  // Replace with the URL to your token's image
 
-            // Add token to MetaMask
-            const wasAdded = await window.ethereum.request({
-                method: 'wallet_watchAsset',
-                params: {
-                    type: 'ERC20',
-                    options: {
-                        address: tokenAddress,
-                        symbol: tokenSymbol,
-                        decimals: tokenDecimals,
-                        image: tokenImage,
+                // Add token to MetaMask
+                const wasAdded = await window.ethereum.request({
+                    method: 'wallet_watchAsset',
+                    params: {
+                        type: 'ERC20',
+                        options: {
+                            address: tokenAddress,
+                            symbol: tokenSymbol,
+                            decimals: tokenDecimals,
+                            image: tokenImage,
+                        },
                     },
-                },
-            });
+                });
 
-            if (wasAdded) {
-                console.log('Token added to MetaMask');
+                if (wasAdded) {
+                    console.log('Token added to MetaMask');
+                } else {
+                    console.log('Token not added to MetaMask');
+                }
             } else {
-                console.log('Token not added to MetaMask');
+                alert('MetaMask is not installed. Please install it first.');
             }
-        } else {
-            alert('MetaMask is not installed. Please install it first.');
+        } catch (error) {
+            console.error(error);
         }
-    } catch (error) {
-        console.error(error);
-    }
+    });
 });
